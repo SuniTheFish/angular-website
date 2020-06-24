af9fc7d5f24df88ba71babf0fffd39e954fb3be8
SuniTheFish <benjamin.hinchliff@gmail.com>
2020-06-23 21:26:48 -0700

    added pipe to add spaces

---
+++ src/app/camel-add-spaces.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { CamelAddSpacesPipe } from './camel-add-spaces.pipe';
+
+describe('CamelAddSpacesPipe', () => {
+  it('create an instance', () => {
+    const pipe = new CamelAddSpacesPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
