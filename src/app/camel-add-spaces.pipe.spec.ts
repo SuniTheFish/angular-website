2d9087c238346c0c3adc00824de999f0f02d459d
SuniTheFish <benjamin.hinchliff@gmail.com>
2020-06-25 21:30:09 -0700

    added test for camel add spaces pipe

--- src/app/camel-add-spaces.pipe.spec.ts
+++ src/app/camel-add-spaces.pipe.spec.ts
@@ -1,8 +1,21 @@
 import { CamelAddSpacesPipe } from './camel-add-spaces.pipe';
 
 describe('CamelAddSpacesPipe', () => {
+  let pipe: CamelAddSpacesPipe;
+
+  beforeEach(() => {
+    pipe = new CamelAddSpacesPipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new CamelAddSpacesPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('adds spaces to camelcase', () => {
+    const initial = 'TheQuickBrownFoxJumpsOverTheLazyDog';
+    expect(pipe.transform(initial)).toBe(
+      'The Quick Brown Fox Jumps Over The Lazy Dog',
+      'does not successfully add spaces'
+    );
   });
 });
